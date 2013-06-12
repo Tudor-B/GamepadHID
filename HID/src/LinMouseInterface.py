c1832e0cc22e04a5c2c13487720aef2fdc2f69ac
Tudor <Tudor@10.140.80.121>
2013-06-12 12:15:38 +0200

    Last debugging for Joypad.py and txt files

--- HID/src/LinMouseInterface.py
+++ HID/src/LinMouseInterface.py
@@ -3,8 +3,9 @@ Created on May 12, 2013
 
 @author: Tudor
 '''
-from pymouse import PyMouse
-m=pyMouse
+#from pymouse import PyMouse
+#m=pyMouse
+from subprocess import Popen, PIPE
 class MouseInterface(object):
     '''
     classdocs
@@ -17,27 +18,38 @@ class MouseInterface(object):
         '''
         pass
     
-    def SetPosition(self, x, y):
-        m.move((x, y))
-        
-    def GetPosition(self):
-        return m.position()
+    def keypress(self,sequence):
+        p = Popen(['xte'], stdin=PIPE)
+        p.communicate(input=sequence)
     
-    def Click_Up(self,argument):
+    def SetPosition(self, x, y):
+        #m.move((x, y))
+        self.keypress("mousemove " + x + " " + y + "\n")
+        
+    #def GetPosition(self):
+        #return m.position()
+    
+    def ClickLeft(self):
         #     def Lclick(x, y):
-        x, y = self.GetPosition()
-        if argument == "left":
-        
-        elif argument =="right":
+        #x, y = self.GetPosition()
+        #m.click(x,y,1)
+        self.keypress("mouseclick " + str(1) + "\n")
          
-    def Click_Down(self,argument):
-        x, y = self.GetPosition()
-        if argument == "left":
+    def ClickRight(self):
+        #x, y = self.GetPosition()
+        #m.click(x,y,2)
+        self.keypress("mouseclick " + str(3) + "\n")
+    
+    def middleDown(self):
+        self.keypress("mousedown " + str(2) + "\n")
         
-        elif argument =="right":
+    def middleUp(self):
+        self.keypress("mouseup " + str(2) + "\n")
         
     def SetRelPos(self, dx, dy ):
-        x, y = self.GetPosition()
-        y = y+6*dy
-        x = x+6*dx
-        self.SetPosition(x, y)
+        #x, y = self.GetPosition()
+        #y = y+6*dy
+        #x = x+6*dx
+        #self.SetPosition(x, y)
+        print "arguments recieved: " + str(dx) + " " + str(dy)
+        self.keypress("mousermove " + str(dx) + " " + str(dy) + "\n")
--- HID/src/joypad.py
+++ HID/src/joypad.py
@@ -1,104 +1,143 @@
+'''
+Created on Jun 4, 2013
+
+@author: Tudor
+'''
 import os
 import pygame
-import win32api, win32con 
-#import win32api
-#import win32con 
-import sys		# Imports pygame library that are made for developing games
+import gui
+import math
+#import sys        # Imports pygame library that are made for developing games
 if os.name == 'nt':
-	from WinMouseInterface import MouseInterface
-	from WinKBInterface import KBInterface
-	pass
+    from Winterface import Interface
+    pass
 elif os.name == 'posix':
-	from LinMouseInterface import MouseInterface
-	from LinKBInterface import KBInterface
-	pass
+    from linterface import Interface
+    pass
+from remapper import remapper
 
-
-
-
-pygame.init()			# Starts pygame
-t = pygame.time.Clock()		# Make a tracker
-keyboard = KBInterface()
-mouse = MouseInterface()
-
+pygame.init()            # Starts pygame
+t = pygame.time.Clock()        # Make a tracker
+interface = Interface()
 os.system('cls' if os.name=='nt' else 'clear') # Clear terminal // remove "there is no soundcard"
 try: 
 
-	print "Number of Devices:", pygame.joystick.get_count()
-	d = pygame.joystick.Joystick(0)
-	d.init()			# Start pygame.joystick.Joystick(0)
-
+    print "Number of Devices:", pygame.joystick.get_count()
+    d = pygame.joystick.Joystick(0)
+    d.init()            # Start pygame.joystick.Joystick(0)
+    rem = remapper(d.get_name())
+    maingui = gui.gui(rem)
 
 # Gives info about the device
-	print "Connected device", d.get_name()
-	print d.get_name(), "have"
-	print "Buttons:", d.get_numbuttons()
-	print "Axes:", d.get_numaxes()
-	print "Hats:", d.get_numhats()
-	print "Balls:", d.get_numballs()
-	print "The device ID is", d.get_id()
-	print "Press button 0 to exit"
+    print "Connected device", d.get_name()
+    print d.get_name(), "have"
+    print "Buttons:", d.get_numbuttons()
+    print "Axes:", d.get_numaxes()
+    print "Hats:", d.get_numhats()
+    print "Balls:", d.get_numballs()
+    print "The device ID is", d.get_id()
+    print "Press button 0 to exit"
 
 # Start showing the info in terminal
 
+    def loop():
+        global d, t
 
+        while pygame.event.get(pygame.QUIT) == []:
+            xy =pygame.event.get(pygame.JOYAXISMOTION) #if this isnt here you cant press buttons?!
+            #xy_True=[] # call the infomation to
+        # Axes Part
 
-	def loop():
-		global d, t
+#             xy_data = []
+#             for c in range(0, d.get_numaxes()):                        #Mouse Movement
+#                 xy_data+= [d.get_axis(c)]
+#             print "axis 1"
+#             print xy_data[0]*20
+#             print "axis 2"
+#             print xy_data[1]*20
+#             interface.SetRelPos(int(float(xy_data[0])), int(float(xy_data[1])))
+            if d.get_name()=="Generic   USB  Joystick  ":
+                interface.SetRelPos(int(math.ceil(d.get_axis(0))*10), int(math.ceil(d.get_axis(1)*10)))
+            else:
+                interface.SetRelPos(int(d.get_axis(0)*20), int(d.get_axis(1)*20))
+            # Button Part Down
+            downbutton = pygame.event.get(pygame.JOYBUTTONDOWN)
+            for button in downbutton:
+                print "Pressed button is", button.button
+                if button.button == rem.buttons['start']:            # The exit button
+                    return
+                elif button.button == int(rem.buttons['select']):
+                    maingui.Menu()
+                    print "AHAHAHA"
+                elif button.button == rem.buttons['up']:
+                    interface.genEvent(rem.actions['up'], 'down')
+                elif button.button == rem.buttons['right']:
+                    interface.genEvent(rem.actions['right'], 'down')
+                elif button.button == rem.buttons['down']:
+                    interface.genEvent(rem.actions['down'], 'down')
+                elif button.button == rem.buttons['left']:
+                    interface.genEvent(rem.actions['left'], 'down')
+                elif button.button == rem.buttons['triangle']:
+                    interface.genEvent(rem.actions['triangle'], 'down')
+                elif button.button == rem.buttons['square']:
+                    interface.genEvent(rem.actions['square'], 'down')
+                elif button.button == rem.buttons['circle']:
+                    interface.genEvent(rem.actions['circle'], 'down')
+                elif button.button == int(rem.buttons['cross']):
+                    interface.genEvent(rem.actions['cross'], 'down')
+                elif button.button == rem.buttons['L1']:
+                    interface.genEvent(rem.actions['L1'], 'down')
+                elif button.button == rem.buttons['L2']:
+                    interface.genEvent(rem.actions['L2'], 'down')
+                elif button.button == rem.buttons['L3']:
+                    interface.genEvent(rem.actions['L3'], 'down')
+                elif button.button == rem.buttons['R1']:
+                    interface.genEvent(rem.actions['R1'], 'down')
+                elif button.button == rem.buttons['R2']:
+                    interface.genEvent(rem.actions['R2'], 'down')
+                elif button.button == rem.buttons['R3']:
+                    interface.genEvent(rem.actions['R3'], 'down')
+                #print (pygame.mouse.get_pos())
 
-		while pygame.event.get(pygame.QUIT) == []:
-			xy =pygame.event.get(pygame.JOYAXISMOTION)
-			#xy_True=[] # call the infomation to
-		# Axes Part
-
-			xy_data = []
-			for c in range(0, d.get_numaxes()):						#Mouse Movement
-				xy_data+= [str(round(d.get_axis(c),1))]
-
-			mouse.SetRelPos(int(float(xy_data[0])), int(float(xy_data[1])))
-
-# 					
-
-			# Button Part Down
-			downbutton = pygame.event.get(pygame.JOYBUTTONDOWN)
-			for button in downbutton:
-				print "Pressed button is", button.button
-				if button.button == 0:			# The exit button
-					return
-				elif button.button == 2:
-					mouse.Click_Down('left')
-				elif button.button == 3:
-					mouse.Click_Down('right')
-				elif button.button == 4:
-					keyboard.KeyUp_Down()
-				elif button.button == 5:
-					keyboard.KeyRight_Down()
-				elif button.button == 6:
-					keyboard.KeyDown_Down()
-				elif button.button == 7:
-					keyboard.KeyLeft_Down()
-
-				print (pygame.mouse.get_pos())
-
-		# Button Part Up
-			upbutton = pygame.event.get(pygame.JOYBUTTONUP)
-			for button in upbutton:
-				print "Button released was", button.button
-				if button.button == 2:
-					mouse.Click_Up('left')
-				elif button.button == 3:
-					mouse.Click_Up('right')
-				elif button.button == 4:
-					keyboard.KeyUp_Up()
-				elif button.button == 5:
-					keyboard.KeyRight_Up()
-				elif button.button == 6:
-					keyboard.KeyDown_Up()
-				elif button.button == 7:
-					keyboard.KeyLeft_Up()
-			t.tick(80)
-	loop()
+            # Button Part Up
+            upbutton = pygame.event.get(pygame.JOYBUTTONUP)
+            for button in upbutton:
+                print "Button released was", button.button
+                if button.button == rem.buttons['start']:            # The exit button
+                    return
+                elif button.button == int(rem.buttons['select']):
+                    interface.genEvent(rem.actions['select'], 'up')
+                elif button.button == rem.buttons['up']:
+                    interface.genEvent(rem.actions['up'], 'up')
+                elif button.button == rem.buttons['right']:
+                    interface.genEvent(rem.actions['right'], 'up')
+                elif button.button == rem.buttons['down']:
+                    interface.genEvent(rem.actions['down'], 'up')
+                elif button.button == rem.buttons['left']:
+                    interface.genEvent(rem.actions['left'], 'up')
+                elif button.button == rem.buttons['triangle']:
+                    interface.genEvent(rem.actions['triangle'], 'up')
+                elif button.button == rem.buttons['square']:
+                    interface.genEvent(rem.actions['square'], 'up')
+                elif button.button == rem.buttons['circle']:
+                    interface.genEvent(rem.actions['circle'], 'up')
+                elif button.button == int(rem.buttons['cross']):
+                    interface.genEvent(rem.actions['cross'], 'up')
+                elif button.button == rem.buttons['L1']:
+                    interface.genEvent(rem.actions['L1'], 'up')
+                elif button.button == rem.buttons['L2']:
+                    interface.genEvent(rem.actions['L2'], 'up')
+                elif button.button == rem.buttons['L3']:
+                    interface.genEvent(rem.actions['L3'], 'up')
+                elif button.button == rem.buttons['R1']:
+                    interface.genEvent(rem.actions['R1'], 'up')
+                elif button.button == rem.buttons['R2']:
+                    interface.genEvent(rem.actions['R2'], 'up')
+                elif button.button == rem.buttons['R3']:
+                    interface.genEvent(rem.actions['R3'], 'up')
+            t.tick(80)
+    loop()
 
 except Exception, e:
-	print "Unhandled exception: %s"%e
-	print "Shutting down now"
+    print "Unhandled exception: %s"%e
+    print "Shutting down now"
